4db1e99107749e896142bab51b5bb0e39e075fd2
vishalparmarr <vishalbusiness3108@gmail.com>
2024-03-31 20:01:07 +0530

    sanity started

---
+++ sanity.config.ts
@@ -0,0 +1,25 @@
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\app\y\[[...index]]\page.tsx` route
+ */
+
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './sanity/env'
+import schemas from './sanity/schemas'
+
+export default defineConfig({
+  basePath: '/studio',
+  projectId,
+  dataset,
+  // Add and edit the content schema in the './sanity/schema' folder
+  schema: {types: schemas},
+  plugins: [
+    structureTool(),
+    // Vision is a tool that lets you query your content with GROQ in the studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
+})
---
+++ sanity/schemas/resource-playlist.schema.ts
@@ -0,0 +1,21 @@
+const schema = {
+  name: "resourcePlaylist",
+  title: "Resource Playlist",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: "resources",
+      title: "Resources",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "resource" }] }],
+    },
+  ],
+};
+
+export default schema;
